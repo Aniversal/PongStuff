1e489f4960d80ccc247813fb238d0eb1838f8d95
GK <gibrilkhatib@gmail.com>
2022-06-09 23:09:32 -0700

    PongDone

---
+++ PONGstuff/Assets/Pong/Ball2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball2 : MonoBehaviour
+{
+    public float speeds;
+    public Rigidbody2D rb2;
+    public Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        Launch();
+    }
+
+    public void Reset()
+    {
+        rb2.velocity = Vector2.zero;
+        transform.position = startPosition;
+        Launch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Launch()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb2.velocity = new Vector2(speeds * x, speeds * y);
+    }
+}
---
+++ PONGstuff/Assets/Pong/Paddle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public bool isPlayer1;
+    public float pSpeed;
+    public Rigidbody2D rb2;
+    public Vector3 startPosition;
+
+    private float movement;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayer1)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            movement = Input.GetAxisRaw("Vertical2");
+        }
+        rb2.velocity = new Vector2(rb2.velocity.x, movement * pSpeed);
+    }
+
+    public void Reset()
+    {
+        rb2.velocity = Vector2.zero;
+        transform.position = startPosition;
+    }
+}
